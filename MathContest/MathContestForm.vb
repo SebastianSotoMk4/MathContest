a0982c3a9306c071c974fd5b99cf40aeb4ae6b68
Sebastian Soto <thefirstcoolone@gmail.com>
2021-10-13 09:16:30 -0600

    save

--- MathContest/MathContestForm.vb
+++ MathContest/MathContestForm.vb
@@ -7,22 +7,47 @@ Option Strict On
 Option Explicit On
 Public Class MathContestForm
     Sub SetDefaults()
+        SecondNumberTextBox.Text = ""
+        StudentAnswerTextBox.Text = ""
+        FirstNumberTextBox.Text = ""
+        NameTextBox.Text = ""
+        AgeTextBox.Text = ""
+        GradeTextBox.Text = ""
+        SubmitButton.Enabled = False
         AddRadioButton.Checked = True
-        ' MathProblemGroupBox.Enabled = False
-        ' MathProblemTypeBox.Enabled = False
-        AddRadioButton.Checked = True
-        SecondNumberTextBox.Text = " "
-        StudentAnswerTextBox.Text = " "
-        FirstNumberTextBox.Text = " "
-        NameTextBox.Text = " "
-        AgeTextBox.Text = " "
-        GradeTextBox.Text = " "
+        'AddRadioButton.Checked = True
+        MathProblemGroupBox.Enabled = False
+        MathProblemTypeBox.Enabled = False
 
     End Sub
+    Function Calculation(ByVal firstNumber As Integer, ByVal secondNumber As Integer, ByVal mathSymbol As String) As Integer
+        Dim answer As Integer
+        firstNumber = RandomNumber(100)
+        secondNumber = RandomNumber(100)
+        FirstNumberTextBox.Text = CStr(firstNumber)
+        SecondNumberTextBox.Text = CStr(secondNumber)
+        Return answer
+    End Function
+    Function CheckAnswer(ByRef answer As Integer, ByRef userAnswer As String, ByRef mesage As String) As String
+        userAnswer = StudentAnswerTextBox.Text
+
+        If userAnswer = CStr(answer) Then
+            mesage = "Goood joshe"
+
+        End If
+
+        MsgBox("gg")
+        Return mesage
+    End Function
+    Function RandomNumber(max As Integer) As Integer
+        Dim rndNum As Double
+        Dim NumRnd As Integer
+        Randomize(Now.Millisecond)
+        rndNum = System.Math.Floor(CDbl(Rnd() * (max)))
+        NumRnd = CInt(rndNum)
+        Return NumRnd
+    End Function
     Sub CheckName()
-        'Dim nameCheck As Boolean
-        'Dim ageCheck As Boolean
-        'Dim gradeCheck As Boolean
 
         If NameTextBox.Text <> "" And AgeTextBox.Text <> "" And GradeTextBox.Text <> "" Then
             SubmitButton.Enabled = True
@@ -33,15 +58,33 @@ Public Class MathContestForm
             MathProblemGroupBox.Enabled = False
             MathProblemTypeBox.Enabled = False
         End If
-        'If AgeTextBox.Text <> "" Then
-        '    ageCheck = True
+    End Sub
+    Private Sub AddRadioButton_CheckedChanged(sender As Object, e As EventArgs) Handles AddRadioButton.CheckedChanged, SubtractRadioButton.CheckedChanged, MultiplyRadioButton.CheckedChanged, DivideRadioButton.CheckedChanged
+        'Calculation(1, 2, "a:")
+        Dim firstNumber As Integer
+        Dim secondNumber As Integer
+        Dim answer As Integer
+        Dim userAnswer As String
+
+        'firstNumber = RandomNumber(3)
+        'secondNumber = RandomNumber(3)
+        'FirstNumberTextBox.Text = CStr(firstNumber)
+        'SecondNumberTextBox.Text = CStr(secondNumber)
+        If SubtractRadioButton.Checked = True Then
+            answer = firstNumber - secondNumber
+
+        End If
+        CheckAnswer(answer, userAnswer, " ")
+
+        'userAnswer = StudentAnswerTextBox.Text
+        'If userAnswer = CStr(answer) Then
+        '    MsgBox("gg")
         'End If
-        'If GradeTextBox.Text <> "" Then
-        '    gradeCheck = True
-        'End If
-        'If nameCheck And ageCheck And gradeCheck = True Then
-        '   
-        'End If
+    End Sub
+    Private Sub MathContestForm_Load(sender As Object, e As EventArgs) Handles Me.Load
+        'On start up the add button is checked to prevent floating inputs
+        SetDefaults()
+
     End Sub
     Private Sub NameTextBox_LostFocus(sender As Object, e As EventArgs) Handles NameTextBox.LostFocus, AgeTextBox.LostFocus, GradeTextBox.LostFocus
         CheckName()
@@ -49,48 +92,15 @@ Public Class MathContestForm
     Private Sub NameTextBox_KeyPress(sender As Object, e As KeyPressEventArgs) Handles NameTextBox.KeyPress, AgeTextBox.KeyPress, GradeTextBox.KeyPress
         CheckName()
     End Sub
-
-    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles FirstNumberLabel.Click
-
-    End Sub
-
-    Private Sub TextBox4_TextChanged(sender As Object, e As EventArgs) Handles FirstNumberTextBox.TextChanged
-
-        'FisrtNumberTextBox = RandomNumber(20)
-    End Sub
-    Private Sub TextBox5_TextChanged(sender As Object, e As EventArgs) Handles SecondNumberTextBox.TextChanged
-
-    End Sub
-
-    Private Sub AddRadioButton_CheckedChanged(sender As Object, e As EventArgs) Handles AddRadioButton.CheckedChanged
-
-    End Sub
-
-    Private Sub MathContestForm_Load(sender As Object, e As EventArgs) Handles Me.Load
-        'On start up the add button is checked to prevent floating inputs
-        SetDefaults()
-
-    End Sub
-
     Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
         Me.Close()
     End Sub
-    Function RandomNumber(max As Integer) As Integer
-        Dim rndNum As Double
-        Dim NumRnd As Integer
-        Randomize(Now.Millisecond)
-        rndNum = System.Math.Floor(CDbl(Rnd() * (max)))
-        NumRnd = CInt(rndNum)
-        Return NumRnd
-    End Function
 
     Private Sub ClearButton_Click(sender As Object, e As EventArgs) Handles ClearButton.Click
         SetDefaults()
     End Sub
 
-    Private Sub NameTextBox_TextChanged(sender As Object, e As EventArgs) Handles NameTextBox.TextChanged
-
+    Private Sub SubmitButton_Click(sender As Object, e As EventArgs) Handles SubmitButton.Click
+        CheckAnswer()
     End Sub
-
-
 End Class
